c14fceb3bc9d0abeb47e56536b5e79ec6d6571b7
Elías <35237918+EliasDario@users.noreply.github.com>
2018-02-02 19:14:20 -0300

    I created the Wagon Class!

---
+++ OregonTrail/src/byui/cit260/oregontrail/model/ResourceScene.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.oregontrail.model;
+import java.io.Serializable;
+/**
+ *
+ * @author Elías
+ */
+public class ResourceScene implements Serializable {
+    
+}
