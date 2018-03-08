7916a400537025671fef257ad2b736e1cddb2171
Amon Bernardo <Amon Bernardo@Amon-PC.Home>
2018-03-08 19:57:10 -0300

    03/08/2017 - ChooseOccupationView

--- OregonTrail/src/byui/cit260/oregontrail/view/ChooseOccupationView.java
+++ OregonTrail/src/byui/cit260/oregontrail/view/ChooseOccupationView.java
@@ -12,53 +12,20 @@ import oregontrail.OregonTrail;
  *
  * @author lucia
  */
-public class ChooseOccupationView {
+public class ChooseOccupationView extends View{
+    public ChooseOccupationView(){
+    super ("\nCHOOSE OCCUPATION:\n" +
+            "1 - Banker from Boston\n" +
+            "2 - Carpenter from Ohio\n" +
+            "3 - Farmer from Illinois\n" +
+            "4 - Which one should I choose\n" +
+            "What is your choice?");
+    }
     
-    public boolean displayChooseOccupationView(){
-        boolean endOfView = false;
-        do {
-        String[] inputs = this.getInputs();
-            if (inputs.equals(null) || inputs[0].toUpperCase().equals("Q")){ 
-                return false;
-            }
-        endOfView = doAction(inputs);    
-            }
-        while (endOfView != true);
-        return true;
-    }
-
-    private String[] getInputs() {
-        String[] inputs = new String[1]; // array one element long
-          System.out.println ("CHOOSE OCCUPATION:\n");
-           System.out.println ("1 - Banker from Boston\n" + 
-                               "2 - Carpenter from Ohio\n"+
-                               "3 - Farmer from Illinois\n"+ 
-                               "4 - Which one should I choose\n");
-          System.out.println("What is your choice?");
-          boolean valid = false;
-          while (valid == false){   // while no input value has been entered
-              System.out.print("Choose one option: ");
-              Scanner userInput = new Scanner(System.in);
-              
-              String userInputted = userInput.nextLine(); //Get the value entered from the keyboard
-              userInputted = userInputted.trim(); //Trim off leading and trailing blanks from the value
-              
-              if (userInputted.length() < 1){
-                 System.out.println("You must choose one option");}
-                 else if ("".equals(userInputted)){
-                         System.out.println("You must choose one occupation");
-                 continue;
-              }
-              inputs[0] = userInputted;
-              valid = true;
-          } 
-          return inputs;
-    }
-
-    private boolean doAction(String[] inputs) {
-        String menuItem = inputs[0];
-        menuItem = menuItem.toUpperCase();
-        switch (menuItem) {
+    @Override
+    public boolean doAction(String value) {
+        value = value.toUpperCase();
+        switch (value) {
             case "1": 
                 System.out.println("Congratulations! You are a banker and have $1600 to start your trip");
                 break;
